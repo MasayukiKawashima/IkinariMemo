470f652ce803ba44511dbbf3c8e2300700832c4b
Masayuki Kawahima <poppinmasa@gmail.com>
2025-07-01 15:03:58 +0900

    プロジェクトファイルの作成完了

---
+++ Child/Child/ChildApp.swift
@@ -0,0 +1,19 @@
+//
+//　アプリの起動処理を定義するファイル
+//　AppDelegate的な役割
+//  ChildApp.swift
+//  Child
+//
+//  Created by 川島真之 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ChildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
