e39ddc9811435555e248b1238d420c864e9f11f7
Masayuki Kawahima <poppinmasa@gmail.com>
2025-07-13 19:33:19 +0900

    Realmの導入とTitleviewでの試用終了

---
+++ Child/Child/Model/UserMemo.swift
@@ -0,0 +1,17 @@
+//
+//  UserMemo.swift
+//  Child
+//
+//  Created by 川島真之 on 2025/07/13.
+//
+
+import Foundation
+import RealmSwift
+
+class UserMemo: Object, ObjectKeyIdentifiable {
+  @Persisted(primaryKey: true) var id: ObjectId
+  @Persisted var title: String = ""
+  @Persisted var content: String = ""
+  @Persisted var createdAt: Date = Date()
+  @Persisted var updatedAt: Date = Date()
+}
