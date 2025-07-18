b4db3ca83ab2f34cef233f64ae29d5d4f64d8006
Masayuki Kawahima <poppinmasa@gmail.com>
2025-07-18 14:58:15 +0900

    CurrentUserMemoManagerを作成した

---
+++ Child/Child/ViewModel/CurrentUserMemoManager.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentUserMemoManager.swift
+//  Child
+//
+//  Created by 川島真之 on 2025/07/18.
+//
+
+import Foundation
+import RealmSwift
+
+final class CurrentUserMemoManager: ObservableObject {
+  
+  // MARK: - Properties
+  // シングルトンとして定義
+  static let shared: CurrentUserMemoManager = .init()
+  @Published var currentUserMemo: UserMemo
+  
+  // MARK: - Init
+  
+  private init() {
+    currentUserMemo = UserMemo()
+  }
+  
+  // MARK: - Methods
+  
+  func upDate(userMemo: UserMemo) {
+    self.currentUserMemo = userMemo
+  }
+  
+}
