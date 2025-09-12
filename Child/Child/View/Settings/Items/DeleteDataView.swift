715561a9993af9242ef6a27c48856069fc7235ba
Masayuki Kawahima <poppinmasa@gmail.com>
2025-09-12 12:15:12 +0900

    データ削除画面とアラートのUIを作成した

--- Child/Child/View/Settings/Items/DeleteDataView.swift
+++ Child/Child/View/Settings/Items/DeleteDataView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct DeleteDataView: View {
+  
+  // MARK: - Properties
+  
+  @State private var isShowAlert = false
+  
+  // MARK: - Body
+  
   var body: some View {
     VStack {
-      Text("DeleteDataview")
+      List {
+        HStack {
+          Text("全てのメモを削除")
+            .foregroundStyle(Color.red)
+          
+          Spacer()
+          
+          Button(action: {
+              // ボタンがタップされた時のアクション
+            isShowAlert.toggle()
+          }) {
+            Image(systemName: "trash.fill")
+          }
+          .alert("警告", isPresented: $isShowAlert) {
+      
+              Button("削除する", role: .destructive) {
+                  // 🔴 削除処理を書く
+                  print("削除実行")
+              }
+              
+              Button("キャンセル", role: .cancel) {
+              }
+          } message: {
+              Text("アプリ内の全てのデータを削除してもよろしいですか\nこの操作は取り消せません")
+          }
+        }
+      }
     }
     .customBackButton()
   }
