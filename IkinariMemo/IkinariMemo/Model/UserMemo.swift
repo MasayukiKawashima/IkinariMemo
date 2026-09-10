0d415098aea80b69af326fa987fffa7b7c966e09
Masayuki Kawahima <poppinmasa@gmail.com>
2026-09-10 13:22:40 +0900

    delete: UserMemoのfunc primaryKey() -> String?を削除

--- IkinariMemo/IkinariMemo/Model/UserMemo.swift
+++ IkinariMemo/IkinariMemo/Model/UserMemo.swift
@@ -15,8 +15,4 @@ class UserMemo: Object, ObjectKeyIdentifiable {
   @Persisted var content: String = ""
   @Persisted var createdAt: Date = Date()
   @Persisted var updatedAt: Date = Date()
-
-  override static func primaryKey() -> String? {
-    return "_id"
-  }
 }
